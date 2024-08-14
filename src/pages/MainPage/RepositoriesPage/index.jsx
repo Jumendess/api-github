d0a4a6488c02d22541697608e52403d7f7ee43a4
Julio Mendes <julio.mendes60@gmail.com>
2024-08-14 13:49:40 -0300

    Atualizando alterações

--- src/pages/MainPage/RepositoriesPage/index.jsx
+++ src/pages/MainPage/RepositoriesPage/index.jsx
@@ -6,10 +6,20 @@ import { Container, Sidebar, Main } from './styles';
 
 
 function ReposotoriesPage() {
+  const user = {
+    login: "Jumendess",
+    name: "Julio Mendes",
+    avatar_url: "https://avatars.githubusercontent.com/u/101362282?v=4",
+    followers: 1,
+    following: 0,
+    company: "Adin",
+    blog: "http://google.com",
+    location: "São Paulo- SP",
+  };
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user} />
         <Filter />
       </Sidebar>
       <Main>
